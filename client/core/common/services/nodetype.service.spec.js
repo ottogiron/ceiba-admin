e69905ec9f97c3f1bd39a98ee2e85bba91a98965
Otto Giron <otto.giron@xoom.com>
2015-02-02 11:41:04 -0600

    Fixed karma tests. Now using requirejs with karma for fron end angular tests

---
+++ client/core/common/services/nodetype.service.spec.js
@@ -0,0 +1,9 @@
+define(['jquery'],function($){
+  describe('just checking karma tests', function(){
+    it('should return something',function(){
+    //  var $el = $('<div></div>');
+    //  $el.html('hola');
+    //  expect($el.text()).toEqual('hola');
+    });
+  });
+});
--- karma.conf.js
+++ karma.conf.js
@@ -1,61 +1,58 @@
 // Karma configuration
-// http://karma-runner.github.io/0.10/config/configuration-file.html
+// Generated on Mon Feb 02 2015 09:58:39 GMT-0600 (CST)
 
 module.exports = function(config) {
   config.set({
-    // base path, that will be used to resolve files and exclude
+
+    // base path that will be used to resolve all patterns (eg. files, exclude)
     basePath: '',
 
-    // testing framework to use (jasmine/mocha/qunit/...)
-    frameworks: ['jasmine'],
+
+    // frameworks to use
+    // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
+    frameworks: ['jasmine', 'requirejs'],
+
 
     // list of files / patterns to load in the browser
     files: [
-      'client/bower_components/jquery/dist/jquery.js',
-      'client/bower_components/angular/angular.js',
-      'client/bower_components/angular-mocks/angular-mocks.js',
-      'client/bower_components/angular-resource/angular-resource.js',
-      'client/bower_components/angular-cookies/angular-cookies.js',
-      'client/bower_components/angular-sanitize/angular-sanitize.js',
-      'client/bower_components/angular-route/angular-route.js',
-      'client/bower_components/angular-bootstrap/ui-bootstrap-tpls.js',
-      'client/bower_components/lodash/dist/lodash.compat.js',
-      'client/bower_components/angular-socket-io/socket.js',
-      'client/bower_components/angular-ui-router/release/angular-ui-router.js',
-      'client/core/app.js',
-      'client/core/app.coffee',
-      'client/core/**/*.js',
-      'client/core/**/*.coffee',
-      'client/components/**/*.js',
-      'client/components/**/*.coffee',
-      'client/core/**/*.jade',
-      'client/components/**/*.jade',
-      'client/core/**/*.html',
-      'client/components/**/*.html'
+      'client/core/test-main.js',
+      { pattern: 'client/bower_components/**/*.js', included: false},
+      { pattern: 'client/core/**/*.js', included: false},
+      { pattern: 'client/core/**/*spec.js', included: false},
+      { pattern: 'client/components/**/*spec.js', included: false}
     ],
 
+
+    // list of files to exclude
+    exclude: [
+      'client/core/requirejs.main.js',
+      'client/bower_components/**/*spec.js',
+      'client/bower_components/**/*Spec.js'
+    ],
+
+
+    // preprocess matching files before serving them to the browser
+    // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
-      '**/*.jade': 'ng-jade2js',
-      '**/*.html': 'html2js',
-      '**/*.coffee': 'coffee',
     },
 
-    ngHtml2JsPreprocessor: {
-      stripPrefix: 'client/'
-    },
 
-    ngJade2JsPreprocessor: {
-      stripPrefix: 'client/'
-    },
+    // test results reporter to use
+    // possible values: 'dots', 'progress'
+    // available reporters: https://npmjs.org/browse/keyword/karma-reporter
+    reporters: ['progress'],
 
-    // list of files / patterns to exclude
-    exclude: [],
 
     // web server port
-    port: 8080,
+    port: 9876,
+
+
+    // enable / disable colors in the output (reporters and logs)
+    colors: true,
+
 
     // level of logging
-    // possible values: LOG_DISABLE || LOG_ERROR || LOG_WARN || LOG_INFO || LOG_DEBUG
+    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
     logLevel: config.LOG_INFO,
 
 
@@ -63,19 +60,13 @@ module.exports = function(config) {
     autoWatch: false,
 
 
-    // Start these browsers, currently available:
-    // - Chrome
-    // - ChromeCanary
-    // - Firefox
-    // - Opera
-    // - Safari (only Mac)
-    // - PhantomJS
-    // - IE (only Windows)
-    browsers: ['PhantomJS'],
+    // start these browsers
+    // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
+    browsers: ['Chrome'],
 
 
     // Continuous Integration mode
-    // if true, it capture browsers, run tests and exit
+    // if true, Karma captures browsers, runs the tests and exits
     singleRun: false
   });
 };
