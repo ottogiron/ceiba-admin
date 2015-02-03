fd547f07731fd9df379fabf95632d1e28669c23c
Otto Giron <otto.giron@xoom.com>
2015-02-03 10:51:17 -0600

    latest work on tree service refactoring

--- client/core/common/services/tree/tree.service.spec.js
+++ client/core/common/services/tree/tree.service.spec.js
@@ -3,18 +3,28 @@ define(['core/common/services/tree/tree.service','ngmock'],function($){
 
 
   describe('TreeService', function(){
-
+    var TreeService, Restangular, $httpBackend;
     var TreeService;
 
     beforeEach(module('jcrSmartAdmin.services'));
 
-    beforeEach(inject(function(_TreeService_,_Restangular_){
+    beforeEach(inject(function(_TreeService_,_$httpBackend_,_Restangular_){
       // The injector unwraps the underscores (_) from around the parameter names when matching
-      TreeService = _TreeService_;    
+      TreeService = _TreeService_;
+      $httpBackend = _$httpBackend_;
+      Restangular = _Restangular_;
     }));
 
     it('should return the root tree in an array',function(done){
+      var rootModel= [{path:'/'}]
+      $httpBackend.expectGET('api/trees').respond(rootModel);
+      
       done();
+    });
+
+    afterEach(function(){
+      $httpBackend.verifyNoOutstandingExpectation();
+      $httpBackend.verifyNoOutstandingRequest();
     });
 
   });
